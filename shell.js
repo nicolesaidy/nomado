7280c70a77f4fd57e09eb662d1bfc3c4d66c2eae
Nicole Saidy <nicolesaidy@gmail.com>
2017-03-23 20:52:00 +0200

    nomado starting

--- shell.js
+++ shell.js
@@ -11,16 +11,3 @@ window.addEventListener('online', function(){
 if (!navigator.onLine){
   offlineMode();
 }
-
-//fetch the json object
-fetch("https://jsonplaceholder.typicode.com/posts")
-.then(function(response){
-  return response.json();
-})
-.then(function(data){
-  var trips = document.querySelector("#trips");
-  trips.innerHTML = "";
-  data.forEach(function(user){
-    trips.innerHTML += "<div class='card'><h3>" + user.title + "</h3>" + "<p>" + user.body + "</p></div>";
-  });
-});
